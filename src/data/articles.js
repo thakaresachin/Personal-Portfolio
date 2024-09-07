54df1ae2e5b62953dff168b8beb673336cce24bd
sachin thakare <thakaresachin789@gmail.com>
2024-09-07 23:58:48 +0530

    all file uploded

---
+++ src/data/articles.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function article_1() {
+	return {
+		
+		
+		
+	};
+}
+
+function article_2() {
+	return {
+		
+	};
+}
+
+const myArticles = [article_1, article_2];
+
+export default myArticles;
